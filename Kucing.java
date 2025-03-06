b74d303ac2f777ec4b5c64a1a81348c141d1cab1
elfaretazd <elfaretadewi@gmail.com>
2025-03-06 12:57:30 +0700

    Kucing.java

---
+++ Kucing.java
@@ -0,0 +1,15 @@
+public class Kucing {
+    // Field
+    public String nama;
+    public String mata;
+    public String bulu;
+
+    // Method
+    public String meow() {
+        return "Meow!";
+    }
+
+    public void purr() {
+        System.out.println("Purr...");
+    }
+}
